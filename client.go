ec31b166ec3ce9f4f4e38701d6042fd60c6c3d9a
Brian Hardock <bhardock@revolv.com>
2014-07-28 10:49:20 -0600

    Refactor Client Text/SMS flow

--- client.go
+++ client.go
@@ -13,26 +13,30 @@ func NewClient(account_sid, auth_token, number string) (*Client, error) {
 	return c, nil
 }
 
-func (c *Client) Text(msg_sms *SMS) (*TwilioResponse, *TwilioError, error) {
+func (c *Client) Text(msg_sms *SMS) (interface{}, error) {
 
 	err := Validate(*msg_sms)
 
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
-	resp, twil_err := Send(c, msg_sms)
-	return resp, twil_err, nil
+	smsResponse := &SMSResponse{}
+
+	resp, err := Send(c, msg_sms, smsResponse)
+	return resp,err 
 }
 
-func (c *Client) Call(msg_voice *Voice) (*TwilioResponse, *TwilioError, error) {
+func (c *Client) Call(msg_voice *CALL) (interface{}, error) {
 
 	err := Validate(*msg_voice)
 
 	if err != nil {
-		return nil, nil, err
+		return nil, err
 	}
 
-	resp, twil_err := Send(c, msg_voice)
-	return resp, twil_err, nil
+	callResponse := &CALLResponse{}
+
+	resp, err := Send(c, msg_voice, callResponse)
+	return resp,err
 }
